637df49789420c4dc91f0e23f79d4f319370289c
Donald Lessells <donald.lessells@gmail.com>
2016-08-08 16:53:48 +0100

    added methods and tests for shop balance, sales and refunds

--- app/Shop.java
+++ app/Shop.java
@@ -4,22 +4,50 @@ public class Shop {
 
   private final String name;
   private int balance;
+  private int salesTotal;
+  private int refundsTotal;
 
   public Shop(String name, int balance){
     this.name = name;
     this.balance = balance;
+    this.salesTotal = 0;
+    this.refundsTotal = 0;
   }
 
   public String getName(){
     return this.name;
   }
-  // following tests commented out as methods being tested are now private
-  // public int getBalance(){
-  //   return this.balance;
-  // }
-  //
-  // public void addFunds(int fundsIn){
-  //   this.balance += fundsIn;
-  // }
+
+  public int getBalance(){
+    return this.balance;
+  }
+
+  private void addFunds(int fundsIn){
+    this.balance += fundsIn;
+  }
+
+  private void subtractFunds(int fundsOut){
+    this.balance -= fundsOut;
+  }
+
+  private void setSalesTotal(int fundsIn){
+    this.salesTotal += fundsIn;
+  }
+
+  private int getSalesTotal(){
+    return this.salesTotal;
+  }
+
+  private void setRefundsTotal(int fundsOut){
+    this.refundsTotal += fundsOut;
+  }
+
+  private int getRefundsTotal(){
+    return this.refundsTotal;
+  }
+
+  private int getBalanceOfSales(){
+    return this.salesTotal - this.refundsTotal;
+  }
 
 }
--- tests/ShopTest.java
+++ tests/ShopTest.java
@@ -18,14 +18,42 @@ public class ShopTest {
   }
 
   @Test
-  private void hasBalance(){
+  public void hasBalance(){
     assertEquals(10000, snoutsRus.getBalance());
   }
 
-  @Test
-  private void canAddToBalance(){
-    snoutsRus.addFunds(10);
-    assertEquals(10010, snoutsRus.getBalance());
-  }
+
+
+  // tests commented out as the following methods are now private: addFunds(), subtractFunds(), setSalesTotal(), getSalesTotal(), setRefundsTotal(), getRefundsTotal(). all tests passed.
+  // @Test
+  // public void canAddToBalance(){
+  //   snoutsRus.addFunds(10);
+  //   assertEquals(10010, snoutsRus.getBalance());
+  // }
+
+  // @Test
+  // public void canSubtractFromBalance(){
+  //   snoutsRus.subtractFunds(6000);
+  //   assertEquals(4000, snoutsRus.getBalance());
+  // }
+
+  // @Test
+  // public void canGetAndSetSalesTotal(){
+  //   snoutsRus.setSalesTotal(5);
+  //   assertEquals(5, snoutsRus.getSalesTotal());
+  // }
+  //
+  // @Test
+  // public void canGetAndSetRefundsTotal(){
+  //   snoutsRus.setRefundsTotal(10);
+  //   assertEquals(10, snoutsRus.getRefundsTotal());
+  // }
+
+  // @Test
+  // public void canReportBalanceOfSales(){
+  //   snoutsRus.setSalesTotal(30);
+  //   snoutsRus.setRefundsTotal(10);
+  //   assertEquals(20, snoutsRus.getBalanceOfSales());
+  // }
 
 }
